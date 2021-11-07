5f4fb3555438a720ed5c9a22d9ce5362f1572ee0
Jhonny millones <jhonny.millones@gmail.com>
2021-11-06 22:00:58 -0500

    refactor(shop): Add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { ShopModule } from './shop/shop.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    ShopModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
