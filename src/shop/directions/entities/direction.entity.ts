581a70ddb509ec9742746ae95876b98ced332b81
Jhonny millones <jhonny.millones@gmail.com>
2021-11-08 03:43:52 -0500

    refactor(direction): Change the relation

--- src/shop/directions/entities/direction.entity.ts
+++ src/shop/directions/entities/direction.entity.ts
@@ -1,6 +1,6 @@
 import { BasicEntity } from 'src/share/entities/basic.entity';
 import { User } from 'src/shop/users/entities/user.entity';
-import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToMany } from 'typeorm';
 
 @Entity()
 export class Direction extends BasicEntity {
@@ -10,7 +10,7 @@ export class Direction extends BasicEntity {
   @Column({ type: 'int' })
   userId: number;
 
-  @OneToOne(
+  @OneToMany(
     () => User,
     user => user.id,
     { nullable: false },
