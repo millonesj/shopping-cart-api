65fa96d7e44d788ee3936b6175b845c00b17b83f
Jhonny millones <jhonny.millones@gmail.com>
2021-11-07 22:53:46 -0500

    chore: Add prefix to uri

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { setupSwagger } from './share/util';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('v1');
   setupSwagger(app);
   await app.listen(3000);
 }
